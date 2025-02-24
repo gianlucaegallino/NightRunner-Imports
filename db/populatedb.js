74c9e660da9514b0ebce5a28e26ff819ef9e258b
giangallino <99291206+gianlucaegallino@users.noreply.github.com>
2025-02-24 11:39:50 -0300

    Fix db population script

--- db/populatedb.js
+++ db/populatedb.js
@@ -74,7 +74,6 @@ INSERT INTO brands (name, year_est, founder) VALUES
 ('Hyundai', 1967, 'Chung Ju-Yung'),
 ('Kia', 1944, 'Kim Cheol-ho');
 
-
 INSERT INTO cars (modelname, brandid, engineid, enginesize, horsepower, torque, weightkg, year, colorid, mileage, drivetrainid, transmissionid, aspirationid) VALUES
 ('Skyline GT-R R34', 3, 2, 2.6, 276, 392, 1560, 1999, 1, 145000, 1, 6, 2),  -- I6, AWD, 6MT, Twin-Turbo
 ('Toyota Supra JZA80 (MK4)', 1, 2, 3.0, 276, 432, 1510, 1998, 2, 190000, 2, 6, 2),  -- I6, RWD, 6MT, Twin-Turbo
@@ -86,13 +85,13 @@ INSERT INTO cars (modelname, brandid, engineid, enginesize, horsepower, torque, 
 ('Nissan Silvia S15 Spec-R', 3, 1, 2.0, 250, 275, 1240, 1999, 8, 140000, 2, 6, 2),  -- I4, RWD, 6MT, Turbo
 ('Honda Integra Type R (DC2)', 2, 1, 1.8, 200, 186, 1120, 1998, 9, 210000, 3, 5, 1),  -- I4, FWD, 5MT, Naturally Aspirated
 ('Mitsubishi GTO Twin Turbo', 6, 4, 3.0, 280, 427, 1740, 1998, 10, 195000, 1, 6, 2),  -- V6, AWD, 6MT, Twin-Turbo
-('Mazda Eunos Cosmo', 4, 3, 2.0, 280, 403, 1570, 1995, 11, 250000, 2, 4, 2),  -- Rotary, RWD, 4AT, Twin-Turbo
-('Nissan 300ZX Twin Turbo (Z32)', 3, 4, 3.0, 280, 388, 1590, 1998, 12, 180000, 2, 5, 2),  -- V6, RWD, 5MT, Twin-Turbo
-('Toyota MR2 GT-S (SW20)', 1, 1, 2.0, 245, 304, 1260, 1998, 13, 160000, 2, 5, 2),  -- I4, RWD, 5MT, Turbo
-('Honda S2000 AP1', 2, 1, 2.0, 250, 217, 1250, 1999, 14, 125000, 2, 6, 1),  -- I4, RWD, 6MT, Naturally Aspirated
-('Nissan Stagea 260RS Autech', 3, 2, 2.6, 276, 393, 1670, 1998, 15, 230000, 1, 5, 2),  -- I6, AWD, 5MT, Twin-Turbo
-('Toyota Celica GT-Four ST205', 1, 1, 2.0, 255, 304, 1390, 1998, 16, 200000, 1, 5, 2),  -- I4, AWD, 5MT, Turbo
-('Suzuki Cappuccino', 7, 6, 0.7, 63, 98, 725, 1997, 17, 170000, 2, 5, 2);  -- I3, RWD, 5MT, Turbo
+('Mazda Eunos Cosmo', 4, 3, 2.0, 280, 403, 1570, 1995, 6, 250000, 2, 4, 2),  -- Rotary, RWD, 4AT, Twin-Turbo
+('Nissan 300ZX Twin Turbo (Z32)', 3, 4, 3.0, 280, 388, 1590, 1998, 8, 180000, 2, 5, 2),  -- V6, RWD, 5MT, Twin-Turbo
+('Toyota MR2 GT-S (SW20)', 1, 1, 2.0, 245, 304, 1260, 1998, 5, 160000, 2, 5, 2),  -- I4, RWD, 5MT, Turbo
+('Honda S2000 AP1', 2, 1, 2.0, 250, 217, 1250, 1999, 3, 125000, 2, 6, 1),  -- I4, RWD, 6MT, Naturally Aspirated
+('Nissan Stagea 260RS Autech', 3, 2, 2.6, 276, 393, 1670, 1998, 8, 230000, 1, 5, 2),  -- I6, AWD, 5MT, Twin-Turbo
+('Toyota Celica GT-Four ST205', 1, 1, 2.0, 255, 304, 1390, 1998, 10, 200000, 1, 5, 2),  -- I4, AWD, 5MT, Turbo
+('Suzuki Cappuccino', 7, 6, 0.7, 63, 98, 725, 1997, 5, 170000, 2, 5, 2);  -- I3, RWD, 5MT, Turbo
 
 `;
 
